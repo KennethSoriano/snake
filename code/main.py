4c9fc4401b7bce09aeb2dd1cb68bab9f60168973
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-12 13:46:02 -0800

    snake setup

--- code/main.py
+++ code/main.py
@@ -1,14 +1,19 @@
 from settings import * 
 
+from snake import Snake
+
 class Main:
     def __init__(self) -> None:
         #general
         pygame.init() 
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Snake')
 
         # game objects
         self.bd_rects = [pygame.Rect((col + int(row%2 == 0)) *CELL_SIZE , row*CELL_SIZE, CELL_SIZE, CELL_SIZE)
                         for col in range(0,COLS,2) for row in range(ROWS)]
+        
+        self.snake = Snake()
 
     def draw_bg(self):
         for rect in self.bd_rects:
@@ -20,9 +25,13 @@ class Main:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+
+                    
             self.display_surface.fill(LIGHT_GREEN)
             self.draw_bg()
+            self.snake.draw()
             pygame.display.update()
 
-main = Main()
-main.run()
+if __name__ == '__main__':
+    main = Main()
+    main.run()
